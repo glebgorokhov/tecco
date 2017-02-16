bb1460058acf77e5dacb2a59bc6bf7cc75b14364
vadim98 <vadimkasyan@yandex.ru>
2017-02-16 18:06:42 +0200

    case.php

---
+++ www/case.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <?php include "blocks/head.php"; ?>
+    <meta name="robots" content="all">
+    <?php include "blocks/meta.php"; ?>
+    <title></title>
+    <?php include "blocks/css.php"; ?>
+</head>
+
+<body> 
+
+<?php include "blocks/sidebar.php"; ?>
+<?php include "blocks/social.php"; ?>
+
+<section class="content case">
+	<div class="content-tabs">
+		<div class="container">
+			<p class="case__name">Venden group</p>
+			<p class="case__sitetype">Corporate website</p>
+			<ul class="case__tags">
+				<li><p>#Responsive design</p></li>
+				<li><p>#Corporate website</p></li>
+				<li><p>#Illustration</p></li>
+			</ul>
+		</div>
+		<a href="cases.php" class="case__backbutton"><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30.39 25.16" fill="#221f1f" width="28"><title>icon_arrow_4</title><path d="M314.94,1583.89a1.51,1.51,0,0,0-.11-0.55v0a1.49,1.49,0,0,0-.32-0.49l-11-11a1.5,1.5,0,1,0-2.12,2.12l8.47,8.47H286.05a1.5,1.5,0,0,0,0,3h23.77l-8.57,8.57a1.5,1.5,0,0,0,2.12,2.12L314.51,1585a1.5,1.5,0,0,0,.32-0.49v0a1.51,1.51,0,0,0,.11-0.55" transform="translate(-284.55 -1571.36)"/></svg>Back to case</a>
+	</div>
+	<div class="case__firstscreen">
+		<img class="case__firstscreenbg" src="img/deer.jpg" alt="@@">
+		<div class="container">
+			<img class="case__logo" src="img/logo_venden_white.svg" alt="@@">
+			<p class="case__subheading">Venden responsive website design and development.</p>
+			<a href="#" class="case__link">Visit Website</a>
+			<p class="case__smalltext">The Discover Moscow portal and mobile app provide Moscow locals and tourists with lots of interesting facts about the capital’s buildings using a large information catalog and dozens of themed routes compiled by renowned experts. A promo website that introduces users to the results of a large study of history of three famous architectural projects in Moscow was created at the studio.</p>
+			<p class="case__text content__text">One of the biggest advantages of the service was an abolity to create special product landing pages, so company can use them in marketing cases.</p>
+		</div>
+	</div>
+	<div class="case__infoblock">
+		<div class="case__image">
+			<img src="img/design_1.jpg" alt="@@">
+		</div>
+		<div class="case__textblock">
+			<p class="case__infotext content__text">Fully responsive design created for 3 different brekpoints allows Venden to show their product in most convenient and good looking way.</p>
+		</div>
+	</div>
+	<div class="case__designblock">
+		<p>Fully responsive design</p>
+		<img src="img/devices.png" alt="@@">
+	</div>
+	<div class="case__infoblock">
+		<div class="case__textblock case__textblock_bottom">
+			<p class="case__infotext content__text">Fully responsive design created for 3 different brekpoints allows Venden to show their product in most convenient and good looking way.</p>
+		</div>
+		<div class="case__image">
+			<img src="img/mineralwater.jpg" alt="@@">
+		</div>
+	</div>
+	<div class="case__slider">
+		<div class="case__wrapper">
+			<div class="case__slide">
+				<img src="img/site.jpg" alt="@@">
+			</div>
+			<div class="case__slide">
+				<img src="img/site.jpg" alt="@@">
+			</div>
+		</div>
+		<div class="case__slidearrows">
+			<div class="prev">
+				<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.62 34.25" width="19" height="35"><title>icon_arrow_5</title><polyline points="1.5 1.5 17.12 17.12 1.5 32.75" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/></svg>
+			</div>
+			<div class="next">
+				<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.62 34.25" width="19" height="35"><title>icon_arrow_5</title><polyline points="1.5 1.5 17.12 17.12 1.5 32.75" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/></svg>
+			</div>
+		</div>
+	</div>
+	<div class="case__bigimage">
+		<img class="case__bg" src="img/nature.jpg" alt="@@">
+		<img class="case__logo case__logo_bottom" src="img/logo_venden_white.svg" alt="@@">
+	</div>
+	<div class="case__about">
+		<div class="container">
+			<div class="case__about__block">
+				<p class="case__about__text case__about__text_strong">About project</p>
+				<p class="case__about__text">Fully responsive design created for 3 different brekpoints allows Venden to show their product in most convenient and good looking way</p>
+			</div>
+			<div class="case__about__block">
+				<p class="case__about__text case__about__text_strong">Issue</p>
+				<p class="case__about__text">Fully responsive design created for 3 different brekpoints allows Venden to show their product in most convenient and good looking way</p>
+			</div>
+			<div class="case__about__block last">
+				<p class="case__about__text case__about__text_strong">Solution</p>
+				<p class="case__about__text">Be creative, be proud of making your own design, and feel the wibe of genetation progress.</p>
+			</div>
+			<ul class="case__about__lists">
+					<li>Проведение исследования</li>
+					<li>Разработка архитектуры, составление технического задания</li>
+					<li>Рзработка прототипа</li>
+					<li>Разработка веб и мобильног интерфейса</li>
+					<li>Front end работы</li>
+					<li>Front end работы</li>
+					<li>QA</li>
+					<li>Суппорт проекта</li>
+				</ul>
+				<p class="case__about__text case__about__text_small">Fully responsive design created 3 different brekpoints allows Venden to show their product in most convenient and good looking way</p>
+		</div>
+	</div>
+	<div class="case__contactblock">
+		<div class="container">
+			<a href="#" class="squareblock squareblock_dark">
+				<p class="content__text"><b>You like the project?</b><br><br>
+				Wana make something as good as this?</p>
+				<p class="content__text content__text_link" href="#">Contact us!
+					<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.16 30.39" width="26" height="31" fill="#221f1f"><title>icon_arrow_3</title><path d="M-47.26,887.51a1.52,1.52,0,0,0-.55.11h0a1.49,1.49,0,0,0-.49.33l-11,11a1.5,1.5,0,0,0,0,2.12,1.5,1.5,0,0,0,2.12,0l8.47-8.47v23.77a1.5,1.5,0,0,0,1.5,1.5,1.5,1.5,0,0,0,1.5-1.5V892.63l8.57,8.57a1.5,1.5,0,0,0,2.12,0,1.49,1.49,0,0,0,.44-1.06,1.5,1.5,0,0,0-.44-1.06L-46.2,888a1.49,1.49,0,0,0-.49-0.33h0a1.52,1.52,0,0,0-.55-0.11" transform="translate(59.8 -887.51)"/></svg>
+				</p>
+				<span class="squareblock__social">Share
+					<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4.5 9.66" width="17" height="34" fill="#221f1f"><defs></defs><title>icon_fb</title><path class="cls-1" d="M-713.19,218.56h1.51l-0.18,1.66h-1.33v4.83h-2v-4.83h-1v-1.66h1v-1a2.31,2.31,0,0,1,.5-1.62,2.16,2.16,0,0,1,1.66-.55h1.33v1.66h-0.83a1.43,1.43,0,0,0-.37,0,0.32,0.32,0,0,0-.2.14,0.56,0.56,0,0,0-.08.2,1.84,1.84,0,0,0,0,.29v0.83Z" transform="translate(716.19 -215.4)"/></svg>
+				</span>
+			</a>
+		</div>
+	</div>
+</section>
+
+<?php include "blocks/forms.php"; ?>
+<?php include "blocks/scripts.php"; ?>
+</body>
+</html>
