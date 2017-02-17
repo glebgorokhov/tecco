54b718faca14779fcefa5476f532213cf9812000
Ilya <rewasd91@mail.ru>
2017-02-17 19:43:16 +0300

    перезаливка

--- www/case.php
+++ www/case.php
@@ -25,7 +25,7 @@
 				<li><p>#Illustration</p></li>
 			</ul>
 		</div>
-		<a href="cases.php" class="case__backbutton"><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30.39 25.16" fill="#221f1f" width="28"><title>icon_arrow_4</title><path d="M314.94,1583.89a1.51,1.51,0,0,0-.11-0.55v0a1.49,1.49,0,0,0-.32-0.49l-11-11a1.5,1.5,0,1,0-2.12,2.12l8.47,8.47H286.05a1.5,1.5,0,0,0,0,3h23.77l-8.57,8.57a1.5,1.5,0,0,0,2.12,2.12L314.51,1585a1.5,1.5,0,0,0,.32-0.49v0a1.51,1.51,0,0,0,.11-0.55" transform="translate(-284.55 -1571.36)"/></svg>Back to case</a>
+		<a href="cases.php" class="case__backbutton"><svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30.39 25.16" fill="#221f1f" width="28"><title>icon_arrow_4</title><path d="M314.94,1583.89a1.51,1.51,0,0,0-.11-0.55v0a1.49,1.49,0,0,0-.32-0.49l-11-11a1.5,1.5,0,1,0-2.12,2.12l8.47,8.47H286.05a1.5,1.5,0,0,0,0,3h23.77l-8.57,8.57a1.5,1.5,0,0,0,2.12,2.12L314.51,1585a1.5,1.5,0,0,0,.32-0.49v0a1.51,1.51,0,0,0,.11-0.55" transform="translate(-284.55 -1571.36)"/></svg>Back to case</a>
 	</div>
 	<div class="case__firstscreen">
 		<img class="case__firstscreenbg" src="img/deer.jpg" alt="@@">
@@ -68,10 +68,10 @@
 		</div>
 		<div class="case__slidearrows">
 			<div class="prev">
-				<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.62 34.25" width="19" height="35"><title>icon_arrow_5</title><polyline points="1.5 1.5 17.12 17.12 1.5 32.75" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/></svg>
+				<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.62 34.25" width="19" height="35"><title>icon_arrow_5</title><polyline points="1.5 1.5 17.12 17.12 1.5 32.75" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/></svg>
 			</div>
 			<div class="next">
-				<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.62 34.25" width="19" height="35"><title>icon_arrow_5</title><polyline points="1.5 1.5 17.12 17.12 1.5 32.75" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/></svg>
+				<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.62 34.25" width="19" height="35"><title>icon_arrow_5</title><polyline points="1.5 1.5 17.12 17.12 1.5 32.75" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/></svg>
 			</div>
 		</div>
 	</div>
@@ -112,10 +112,10 @@
 				<p class="content__text"><b>You like the project?</b><br><br>
 				Wana make something as good as this?</p>
 				<p class="content__text content__text_link" href="#">Contact us!
-					<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.16 30.39" width="26" height="31" fill="#221f1f"><title>icon_arrow_3</title><path d="M-47.26,887.51a1.52,1.52,0,0,0-.55.11h0a1.49,1.49,0,0,0-.49.33l-11,11a1.5,1.5,0,0,0,0,2.12,1.5,1.5,0,0,0,2.12,0l8.47-8.47v23.77a1.5,1.5,0,0,0,1.5,1.5,1.5,1.5,0,0,0,1.5-1.5V892.63l8.57,8.57a1.5,1.5,0,0,0,2.12,0,1.49,1.49,0,0,0,.44-1.06,1.5,1.5,0,0,0-.44-1.06L-46.2,888a1.49,1.49,0,0,0-.49-0.33h0a1.52,1.52,0,0,0-.55-0.11" transform="translate(59.8 -887.51)"/></svg>
+					<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.16 30.39" width="26" height="31" fill="#221f1f"><title>icon_arrow_3</title><path d="M-47.26,887.51a1.52,1.52,0,0,0-.55.11h0a1.49,1.49,0,0,0-.49.33l-11,11a1.5,1.5,0,0,0,0,2.12,1.5,1.5,0,0,0,2.12,0l8.47-8.47v23.77a1.5,1.5,0,0,0,1.5,1.5,1.5,1.5,0,0,0,1.5-1.5V892.63l8.57,8.57a1.5,1.5,0,0,0,2.12,0,1.49,1.49,0,0,0,.44-1.06,1.5,1.5,0,0,0-.44-1.06L-46.2,888a1.49,1.49,0,0,0-.49-0.33h0a1.52,1.52,0,0,0-.55-0.11" transform="translate(59.8 -887.51)"/></svg>
 				</p>
 				<span class="squareblock__social">Share
-					<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4.5 9.66" width="17" height="34" fill="#221f1f"><defs></defs><title>icon_fb</title><path class="cls-1" d="M-713.19,218.56h1.51l-0.18,1.66h-1.33v4.83h-2v-4.83h-1v-1.66h1v-1a2.31,2.31,0,0,1,.5-1.62,2.16,2.16,0,0,1,1.66-.55h1.33v1.66h-0.83a1.43,1.43,0,0,0-.37,0,0.32,0.32,0,0,0-.2.14,0.56,0.56,0,0,0-.08.2,1.84,1.84,0,0,0,0,.29v0.83Z" transform="translate(716.19 -215.4)"/></svg>
+					<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 4.5 9.66" width="17" height="34" fill="#221f1f"><defs></defs><title>icon_fb</title><path class="cls-1" d="M-713.19,218.56h1.51l-0.18,1.66h-1.33v4.83h-2v-4.83h-1v-1.66h1v-1a2.31,2.31,0,0,1,.5-1.62,2.16,2.16,0,0,1,1.66-.55h1.33v1.66h-0.83a1.43,1.43,0,0,0-.37,0,0.32,0.32,0,0,0-.2.14,0.56,0.56,0,0,0-.08.2,1.84,1.84,0,0,0,0,.29v0.83Z" transform="translate(716.19 -215.4)"/></svg>
 				</span>
 			</a>
 		</div>
